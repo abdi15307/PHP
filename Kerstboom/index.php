ee29e12eb0c2ba6d468a48bd40fa7f1500ea0854
abdi15307 <said.abdihaqmohamed@student.rocva.nl>
2017-02-08 13:12:15 +0100

    h/w

---
+++ Kerstboom/index.php
@@ -0,0 +1,15 @@
+<center>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S.A. Mohamed
+ * Date: 8-2-2017
+ * Time: 13:01
+ */
+$regel = 10;
+for ($i = 1; $i <= $regel; $i++){
+    for ($j = 0; $j < $i;$j++){
+        echo '*';
+    }
+    echo "<br>";
+}
